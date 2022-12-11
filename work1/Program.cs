150bc8af910b45528c0e9269d9dc3aa369a26a33
Vasilev Kirill <kirya-vasilev-1994@mail.ru>
2022-12-11 16:09:46 +0300

    Добавил домашнюю работу по шестому семинару

---
+++ work1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №41. Программа, которая считает сколько чисел больше 0 ввёл пользователь
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов > 0: {count}");
